0a5c76aa9e720a341cf5bdf21d0039356f4b968c
MrDobler <arthur.dobler@techx.eng.br>
2019-06-05 17:53:56 -0300

    Add Saving and removing; Add component function sharing

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import { Header } from './components/header/Header';
-import Form from './components/form/Form';
 import List from './components/list/List';
 
 
+
 class App extends Component {
+
     render() {
         return (
             <div className="App">
                 <Header header={{ title: 'Projeto Boladão', number: 666 }}/>
                 <div className="container">
                     <List></List>
-                    <Form></Form>                    
                 </div>
             </div>
         );
--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,20 +1,28 @@
 import React from 'react';
-import FormService from '../form/service/FormService.js';
+import UserService from '../form/service/UserService.js';
 import { FaSearch } from 'react-icons/fa';
-import { IoIosMale, IoIosFemale } from 'react-icons/io';
 import Loader from '../loader/Loader.js';
-import { FaArrowRight, FaArrowLeft } from 'react-icons/fa';
+import Form from '../../components/form/Form';
+import { FaArrowRight, FaArrowLeft, FaRegEdit, FaRegTrashAlt } from 'react-icons/fa';
 
 export default class List extends React.Component {
     users = [];
-    formService = new FormService();
-    state = {
-        users: [],
-        loading: true
+    userService = new UserService();
+
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            users: [],
+            loading: true,
+            getUsers: this.getUsers,
+            pagination: [],
+            meta: [],
+            profileCombo: [],
+            editUser: null,
+        };
     }
 
-
-    
     upperFirstLetter(name) {
         return name.split(' ').map((slice) =>  slice.charAt(0).toUpperCase() + slice.slice(1) + " " );
     }
@@ -23,89 +31,120 @@ export default class List extends React.Component {
         this.getUsers();
     }
 
-    handlePageClick = (event, value, callback) => {
-        callback(value);
-    }
-    
-    getUsers(page) {
-         this.formService.all(page)
-                    .then(res => {
-                       this.setState({ loading: false, users: res });
-                       console.log(this.state.users);
-                    })
-                   .catch(err => console.log(err));
+    handlePageClick = (event, value) => {
+        this.setState({
+            getUsers: this.getUsers(value)
+        });
     }
 
-    getSymbolByGender(gender) {
-        return (gender === 'male') ? <IoIosMale/> : <IoIosFemale/>
+    reloadComponent() {
+        this.setState({
+            loading: true,
+            update: this.getUsers(),
+        });
+    }
+    
+    getUsers() {
+        this.userService.all()
+            .then(res => {
+                this.setState({ 
+                    loading: false,
+                    users: res.data,
+                    pagination: res.links,
+                    meta: res.meta 
+                });
+                console.log(res);
+            })
+            .catch(err => console.log(err));
+    }
+
+    deleteUser = (event, user_id) => {
+        this.userService.delete(user_id)
+            .then(() => {
+                this.reloadComponent();
+            })
+            .catch(err => {alert('Erro'); console.log(err)})
+    }
+
+    setEditUser = (event, user) => {
+        console.log('editou');
+        this.setState({ editUser: user })
     }
 
     render() {
-        
 
         return (
-            <div className="card" style={{ marginTop: 25 }}>
-                <h1>Usuários</h1>
-                
-              {this.state.loading ? ( <div className="card-body"> <Loader/> </div> ) : (
-                <div className="card-body">
-                    <div className="col-md-6 offset-md-3">
-                        <div className="input-group mb-3">
-                            <input type="text" className="form-control" placeholder="Buscar usuário" aria-label="Buscar usuário" aria-describedby="basic-addon2"/>
-                            <div className="input-group-append">
-                                <button className="btn btn-outline-secondary" type="button"><FaSearch/></button>
+
+            <div>
+                <div className="card" style={{ marginTop: 25 }}>
+                    <h1>Usuários</h1>
+                    
+                {this.state.loading ? ( <div className="card-body"> <Loader/> </div> ) : (
+                    <div className="card-body">
+                        <div className="col-md-6 offset-md-3">
+                            <div className="input-group mb-3">
+                                <input type="text" className="form-control" placeholder="Buscar usuário" aria-label="Buscar usuário" aria-describedby="basic-addon2"/>
+                                <div className="input-group-append">
+                                    <button className="btn btn-outline-secondary" type="button"><FaSearch/></button>
+                                </div>
                             </div>
                         </div>
+
+
+                        <table className="table table-sm">
+                            <thead>
+                                <tr>
+                                    <th scolpe="col">#</th>
+                                    <th scope="col">Nome</th>
+                                    <th scope="col">E-mail</th>
+                                    <th scope="col">Perfil</th>
+                                    <th scope="col">Status</th>
+                                    <th scope="col">Ações</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                { 
+                                    this.state.users.map((user) => (
+                                        <tr key={user.id}>
+                                            <td>{user.id}</td>
+                                            <td>{user.name}</td>
+                                            <td>{user.email}</td>
+                                            <td>{user.profile.name}</td>
+                                            <td>{user.active ? "Ativo" : "Inativo"}</td>
+                                            <td>
+                                                <button className="btn btn-default" onClick={(evt) => this.setEditUser(evt, user)}><FaRegEdit/></button>
+                                                <button className="btn btn-default" onClick={(evt) => this.deleteUser(evt, user.id)}><FaRegTrashAlt/></button>
+                                            </td>
+                                        </tr>
+                                    ))
+                                }
+                            </tbody>
+                        </table>
+
+                        <div>
+                            <nav style={{ display: 'flex', justifyContent: 'center'}} className="" aria-label="...">
+                                <ul className="pagination">
+                                    <li className="page-item disabled">
+                                        <button className="page-link" ><FaArrowLeft/></button>
+                                    </li>                              
+                                    <li className="page-item active"><button className="page-link"  onClick={(evt) => this.handlePageClick(evt, 1)}>1</button></li>            
+                                    <li className="page-item">
+                                        <button className="page-link" onClick={(evt) => this.handlePageClick(evt, 2)}>2 <span className="sr-only">(current)</span></button>
+                                    </li>
+                                    <li className="page-item"><button className="page-link" onClick={(evt) => this.handlePageClick(evt, 3)}>3</button></li>
+                                    <li className="page-item">
+                                        <button className="page-link"><FaArrowRight/></button>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+
+                        <button className="btn btn-large btn-dark" onClick={this.reloadComponent.bind(this)}>Atualizar</button>
                     </div>
-
-
-                    <table className="table table-sm">
-                        <thead>
-                            <tr>
-                                <th scolpe="col">#</th>
-                                <th scope="col">Nome</th>
-                                <th scope="col">Sobrenome</th>
-                                <th scope="col">E-mail</th>
-                                <th scope="col">Telefone</th>                            
-                                <th scope="col">Cidade</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            { 
-                                this.state.users.map((user) => (
-                                    <tr key={user.id}>
-                                        <td><img style={{ borderRadius: 50, width: 30}} src={user.picture} /></td>
-                                        <td>{user.name}</td>
-                                        <td>{user.last_name}</td>
-                                        <td>{user.email}</td>
-                                        <td>{user.phone}</td>                                    
-                                        <td>{user.city}</td>
-                                    </tr>
-                                ))
-                            }
-                        </tbody>
-                    </table>
-
-                    <div>
-                        <nav style={{ display: 'flex', justifyContent: 'center'}} className="" aria-label="...">
-                            <ul className="pagination">
-                                <li className="page-item disabled">
-                                    <button className="page-link" ><FaArrowLeft/></button>
-                                </li>
-                                <li className="page-item active"><button className="page-link"  onClick={(evt) => this.handlePageClick(evt, 1, this.getUsers)}>1</button></li>
-                                <li className="page-item">
-                                    <button className="page-link" onClick={(evt) => this.handlePageClick(evt, 2, this.getUsers)}>2 <span className="sr-only">(current)</span></button>
-                                </li>
-                                <li className="page-item"><button className="page-link" onClick={(evt) => this.handlePageClick(evt, 3, this.getUsers)}>3</button></li>
-                                <li className="page-item">
-                                    <button className="page-link"><FaArrowRight/></button>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
+                )}
 
                 </div>
-             )}
+                <Form  user={this.state.editUser || null} update={this.reloadComponent.bind(this)} ></Form>
             </div>
         );
     }
