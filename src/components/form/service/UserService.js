7a18a8a18a0014aed2ddde83032d7a7ef514b9dc
Arthur Dobler <arthur.dobler@gmail.com>
2019-06-06 00:19:49 +0000

    Placing authorization inside a var

--- src/components/form/service/UserService.js
+++ src/components/form/service/UserService.js
@@ -1,16 +1,11 @@
 export default class UserService {
-    myHdrs = new Headers({
-        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjQ5NWE4M2Y4NjM3MjlhMWY3YzIxZWY4NjMxMzFmZTEwZjI0ZTczOTMyOWYyMzkyZWU3ZmFhZmY0MDI1ODY2MDU0YTdjMGRiMDMwMWI5ZDgyIn0.eyJhdWQiOiIyIiwianRpIjoiNDk1YTgzZjg2MzcyOWExZjdjMjFlZjg2MzEzMWZlMTBmMjRlNzM5MzI5ZjIzOTJlZTdmYWFmZjQwMjU4NjYwNTRhN2MwZGIwMzAxYjlkODIiLCJpYXQiOjE1NTk3NjQzMDAsIm5iZiI6MTU1OTc2NDMwMCwiZXhwIjoxNTkxMzg2NzAwLCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.rNYK_nz5Nqk8ijc0EpbDU85iNmVyosuDbJs2sjkj5VlutVyMNlPaX3mUfozGdDCVAhCHTXSvM2Av4nIDBAeGAGvwPe-BntbgeZRfkn4S3BR4DxybCcSPZq8XyJwk87XLY3bC182urUmJofpT9s47WxvJMBOz-SAQeIQD55RINkyoNxD_SGmfx3hsG5gvJAh-0h5zIrCHV1V230rN8RoidKY5TiS-3TG9gStDmibii1M8FhQ_Dkwz_-OFvYjjUNN8SNaJqxg0HPqXfzm6NTVtZOXsZrL8iZJx155Wl1BfPG2pdPJMQdviGGvGNFSUwvhHbcP7hW_wUhiFDw9YZDn7LfkD3TIr4nJT0JEOiFCDfnjgf8dGs5S_dB6d6s2IxzOxBn0Gj_W8jLq90FiSbJgpCa3eDeM-1j9V73s0pK2878w3zTxgAodvNoZu7Oo17TRhxFvryuvYWDkNHHVivdhvrGFIL4M4FNIZdbbmFCu3Z-vl1xlXgDRYElwQr2_AKkJNy2LHyK3xPA8AM6OgSifPyoMG33nzI_noHHaJfHPGOUJ3mjMHiY722Ucwq_MKbIPev87drsNTUiQ54soTo7L9cROIZA_N4HpUyR_YUPW-1uNdCpZgsnjZOtQcyAPrQwxzC7DEY3xECgZ0J8xERY2X9Ssg-aM8_PGn253xfVh9p4A',
-        'Content-Type': 'application/json',
-    });
+    authorization = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjQ5NWE4M2Y4NjM3MjlhMWY3YzIxZWY4NjMxMzFmZTEwZjI0ZTczOTMyOWYyMzkyZWU3ZmFhZmY0MDI1ODY2MDU0YTdjMGRiMDMwMWI5ZDgyIn0.eyJhdWQiOiIyIiwianRpIjoiNDk1YTgzZjg2MzcyOWExZjdjMjFlZjg2MzEzMWZlMTBmMjRlNzM5MzI5ZjIzOTJlZTdmYWFmZjQwMjU4NjYwNTRhN2MwZGIwMzAxYjlkODIiLCJpYXQiOjE1NTk3NjQzMDAsIm5iZiI6MTU1OTc2NDMwMCwiZXhwIjoxNTkxMzg2NzAwLCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.rNYK_nz5Nqk8ijc0EpbDU85iNmVyosuDbJs2sjkj5VlutVyMNlPaX3mUfozGdDCVAhCHTXSvM2Av4nIDBAeGAGvwPe-BntbgeZRfkn4S3BR4DxybCcSPZq8XyJwk87XLY3bC182urUmJofpT9s47WxvJMBOz-SAQeIQD55RINkyoNxD_SGmfx3hsG5gvJAh-0h5zIrCHV1V230rN8RoidKY5TiS-3TG9gStDmibii1M8FhQ_Dkwz_-OFvYjjUNN8SNaJqxg0HPqXfzm6NTVtZOXsZrL8iZJx155Wl1BfPG2pdPJMQdviGGvGNFSUwvhHbcP7hW_wUhiFDw9YZDn7LfkD3TIr4nJT0JEOiFCDfnjgf8dGs5S_dB6d6s2IxzOxBn0Gj_W8jLq90FiSbJgpCa3eDeM-1j9V73s0pK2878w3zTxgAodvNoZu7Oo17TRhxFvryuvYWDkNHHVivdhvrGFIL4M4FNIZdbbmFCu3Z-vl1xlXgDRYElwQr2_AKkJNy2LHyK3xPA8AM6OgSifPyoMG33nzI_noHHaJfHPGOUJ3mjMHiY722Ucwq_MKbIPev87drsNTUiQ54soTo7L9cROIZA_N4HpUyR_YUPW-1uNdCpZgsnjZOtQcyAPrQwxzC7DEY3xECgZ0J8xERY2X9Ssg-aM8_PGn253xfVh9p4A';
 
-    
-    
     async all(page = 1) {
         return fetch ('http://dev.api.gsci.techx.eng.br/api/user', {
             method: 'GET',
             headers: new Headers({
-                'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjQ5NWE4M2Y4NjM3MjlhMWY3YzIxZWY4NjMxMzFmZTEwZjI0ZTczOTMyOWYyMzkyZWU3ZmFhZmY0MDI1ODY2MDU0YTdjMGRiMDMwMWI5ZDgyIn0.eyJhdWQiOiIyIiwianRpIjoiNDk1YTgzZjg2MzcyOWExZjdjMjFlZjg2MzEzMWZlMTBmMjRlNzM5MzI5ZjIzOTJlZTdmYWFmZjQwMjU4NjYwNTRhN2MwZGIwMzAxYjlkODIiLCJpYXQiOjE1NTk3NjQzMDAsIm5iZiI6MTU1OTc2NDMwMCwiZXhwIjoxNTkxMzg2NzAwLCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.rNYK_nz5Nqk8ijc0EpbDU85iNmVyosuDbJs2sjkj5VlutVyMNlPaX3mUfozGdDCVAhCHTXSvM2Av4nIDBAeGAGvwPe-BntbgeZRfkn4S3BR4DxybCcSPZq8XyJwk87XLY3bC182urUmJofpT9s47WxvJMBOz-SAQeIQD55RINkyoNxD_SGmfx3hsG5gvJAh-0h5zIrCHV1V230rN8RoidKY5TiS-3TG9gStDmibii1M8FhQ_Dkwz_-OFvYjjUNN8SNaJqxg0HPqXfzm6NTVtZOXsZrL8iZJx155Wl1BfPG2pdPJMQdviGGvGNFSUwvhHbcP7hW_wUhiFDw9YZDn7LfkD3TIr4nJT0JEOiFCDfnjgf8dGs5S_dB6d6s2IxzOxBn0Gj_W8jLq90FiSbJgpCa3eDeM-1j9V73s0pK2878w3zTxgAodvNoZu7Oo17TRhxFvryuvYWDkNHHVivdhvrGFIL4M4FNIZdbbmFCu3Z-vl1xlXgDRYElwQr2_AKkJNy2LHyK3xPA8AM6OgSifPyoMG33nzI_noHHaJfHPGOUJ3mjMHiY722Ucwq_MKbIPev87drsNTUiQ54soTo7L9cROIZA_N4HpUyR_YUPW-1uNdCpZgsnjZOtQcyAPrQwxzC7DEY3xECgZ0J8xERY2X9Ssg-aM8_PGn253xfVh9p4A',
+                'Authorization': this.authorization,
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
             }),
@@ -34,7 +29,7 @@ export default class UserService {
             method: 'POST',
             body: JSON.stringify(user),
             headers: new Headers({
-                'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjQ5NWE4M2Y4NjM3MjlhMWY3YzIxZWY4NjMxMzFmZTEwZjI0ZTczOTMyOWYyMzkyZWU3ZmFhZmY0MDI1ODY2MDU0YTdjMGRiMDMwMWI5ZDgyIn0.eyJhdWQiOiIyIiwianRpIjoiNDk1YTgzZjg2MzcyOWExZjdjMjFlZjg2MzEzMWZlMTBmMjRlNzM5MzI5ZjIzOTJlZTdmYWFmZjQwMjU4NjYwNTRhN2MwZGIwMzAxYjlkODIiLCJpYXQiOjE1NTk3NjQzMDAsIm5iZiI6MTU1OTc2NDMwMCwiZXhwIjoxNTkxMzg2NzAwLCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.rNYK_nz5Nqk8ijc0EpbDU85iNmVyosuDbJs2sjkj5VlutVyMNlPaX3mUfozGdDCVAhCHTXSvM2Av4nIDBAeGAGvwPe-BntbgeZRfkn4S3BR4DxybCcSPZq8XyJwk87XLY3bC182urUmJofpT9s47WxvJMBOz-SAQeIQD55RINkyoNxD_SGmfx3hsG5gvJAh-0h5zIrCHV1V230rN8RoidKY5TiS-3TG9gStDmibii1M8FhQ_Dkwz_-OFvYjjUNN8SNaJqxg0HPqXfzm6NTVtZOXsZrL8iZJx155Wl1BfPG2pdPJMQdviGGvGNFSUwvhHbcP7hW_wUhiFDw9YZDn7LfkD3TIr4nJT0JEOiFCDfnjgf8dGs5S_dB6d6s2IxzOxBn0Gj_W8jLq90FiSbJgpCa3eDeM-1j9V73s0pK2878w3zTxgAodvNoZu7Oo17TRhxFvryuvYWDkNHHVivdhvrGFIL4M4FNIZdbbmFCu3Z-vl1xlXgDRYElwQr2_AKkJNy2LHyK3xPA8AM6OgSifPyoMG33nzI_noHHaJfHPGOUJ3mjMHiY722Ucwq_MKbIPev87drsNTUiQ54soTo7L9cROIZA_N4HpUyR_YUPW-1uNdCpZgsnjZOtQcyAPrQwxzC7DEY3xECgZ0J8xERY2X9Ssg-aM8_PGn253xfVh9p4A',
+                'Authorization': this.authorization,
                 'Content-Type': 'application/json',
             }),
             mode: 'cors',
@@ -48,7 +43,7 @@ export default class UserService {
         return fetch ('http://dev.api.gsci.techx.eng.br/api/user/'+id, {
             method: 'DELETE',            
             headers: new Headers({
-                'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjQ5NWE4M2Y4NjM3MjlhMWY3YzIxZWY4NjMxMzFmZTEwZjI0ZTczOTMyOWYyMzkyZWU3ZmFhZmY0MDI1ODY2MDU0YTdjMGRiMDMwMWI5ZDgyIn0.eyJhdWQiOiIyIiwianRpIjoiNDk1YTgzZjg2MzcyOWExZjdjMjFlZjg2MzEzMWZlMTBmMjRlNzM5MzI5ZjIzOTJlZTdmYWFmZjQwMjU4NjYwNTRhN2MwZGIwMzAxYjlkODIiLCJpYXQiOjE1NTk3NjQzMDAsIm5iZiI6MTU1OTc2NDMwMCwiZXhwIjoxNTkxMzg2NzAwLCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.rNYK_nz5Nqk8ijc0EpbDU85iNmVyosuDbJs2sjkj5VlutVyMNlPaX3mUfozGdDCVAhCHTXSvM2Av4nIDBAeGAGvwPe-BntbgeZRfkn4S3BR4DxybCcSPZq8XyJwk87XLY3bC182urUmJofpT9s47WxvJMBOz-SAQeIQD55RINkyoNxD_SGmfx3hsG5gvJAh-0h5zIrCHV1V230rN8RoidKY5TiS-3TG9gStDmibii1M8FhQ_Dkwz_-OFvYjjUNN8SNaJqxg0HPqXfzm6NTVtZOXsZrL8iZJx155Wl1BfPG2pdPJMQdviGGvGNFSUwvhHbcP7hW_wUhiFDw9YZDn7LfkD3TIr4nJT0JEOiFCDfnjgf8dGs5S_dB6d6s2IxzOxBn0Gj_W8jLq90FiSbJgpCa3eDeM-1j9V73s0pK2878w3zTxgAodvNoZu7Oo17TRhxFvryuvYWDkNHHVivdhvrGFIL4M4FNIZdbbmFCu3Z-vl1xlXgDRYElwQr2_AKkJNy2LHyK3xPA8AM6OgSifPyoMG33nzI_noHHaJfHPGOUJ3mjMHiY722Ucwq_MKbIPev87drsNTUiQ54soTo7L9cROIZA_N4HpUyR_YUPW-1uNdCpZgsnjZOtQcyAPrQwxzC7DEY3xECgZ0J8xERY2X9Ssg-aM8_PGn253xfVh9p4A',
+                'Authorization': this.authorization,
                 'Content-Type': 'application/json',
             }),
             mode: 'cors',
@@ -62,7 +57,7 @@ export default class UserService {
         return fetch ('http://dev.api.gsci.techx.eng.br/api/user/'+id, {
             method: 'DELETE',            
             headers: new Headers({
-                'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjQ5NWE4M2Y4NjM3MjlhMWY3YzIxZWY4NjMxMzFmZTEwZjI0ZTczOTMyOWYyMzkyZWU3ZmFhZmY0MDI1ODY2MDU0YTdjMGRiMDMwMWI5ZDgyIn0.eyJhdWQiOiIyIiwianRpIjoiNDk1YTgzZjg2MzcyOWExZjdjMjFlZjg2MzEzMWZlMTBmMjRlNzM5MzI5ZjIzOTJlZTdmYWFmZjQwMjU4NjYwNTRhN2MwZGIwMzAxYjlkODIiLCJpYXQiOjE1NTk3NjQzMDAsIm5iZiI6MTU1OTc2NDMwMCwiZXhwIjoxNTkxMzg2NzAwLCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.rNYK_nz5Nqk8ijc0EpbDU85iNmVyosuDbJs2sjkj5VlutVyMNlPaX3mUfozGdDCVAhCHTXSvM2Av4nIDBAeGAGvwPe-BntbgeZRfkn4S3BR4DxybCcSPZq8XyJwk87XLY3bC182urUmJofpT9s47WxvJMBOz-SAQeIQD55RINkyoNxD_SGmfx3hsG5gvJAh-0h5zIrCHV1V230rN8RoidKY5TiS-3TG9gStDmibii1M8FhQ_Dkwz_-OFvYjjUNN8SNaJqxg0HPqXfzm6NTVtZOXsZrL8iZJx155Wl1BfPG2pdPJMQdviGGvGNFSUwvhHbcP7hW_wUhiFDw9YZDn7LfkD3TIr4nJT0JEOiFCDfnjgf8dGs5S_dB6d6s2IxzOxBn0Gj_W8jLq90FiSbJgpCa3eDeM-1j9V73s0pK2878w3zTxgAodvNoZu7Oo17TRhxFvryuvYWDkNHHVivdhvrGFIL4M4FNIZdbbmFCu3Z-vl1xlXgDRYElwQr2_AKkJNy2LHyK3xPA8AM6OgSifPyoMG33nzI_noHHaJfHPGOUJ3mjMHiY722Ucwq_MKbIPev87drsNTUiQ54soTo7L9cROIZA_N4HpUyR_YUPW-1uNdCpZgsnjZOtQcyAPrQwxzC7DEY3xECgZ0J8xERY2X9Ssg-aM8_PGn253xfVh9p4A',
+                'Authorization': this.authorization,
                 'Content-Type': 'application/json',
             }),
             mode: 'cors',
